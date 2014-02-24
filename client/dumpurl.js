9e501b16f2066d07cab505de4c29f825ce2ae9f1
Jason White <jdwhite@menelos.com>
2014-02-24 09:13:15 -0600

    Cleaned up formatting, comments. Now explicitly calls phantom.exit() at the end.

--- client/dumpurl.js
+++ client/dumpurl.js
@@ -1,7 +1,7 @@
 //
 // dumpurl.js - fetch, render, and dump HTML to stdout
 //
-// Jason White
+// Jason White <jdwhite@meneos.com>
 // 11-Feb-2014
 //
 var page = new WebPage(), address;
@@ -14,21 +14,22 @@ if (phantom.args.length === 0) {
 // http://stackoverflow.com/questions/16854788/phantomjs-webpage-timeout
 page.settings.resourceTimeout = 10000; // 10 seconds
 page.onResourceTimeout = function(e) {
-  console.log(e.errorCode);   // it'll probably be 408
-  console.log(e.errorString); // it'll probably be 'Network timeout on resource'
-  console.log(e.url);         // the url whose request timed out
-  phantom.exit(1);
+    console.log(e.errorCode);   // Probably be 408
+    console.log(e.errorString); // Probably be 'Network timeout on resource'
+    console.log(e.url);         // URL whose request timed out
+    phantom.exit(1);
 };
 
 address = encodeURI(phantom.args[0]);
 page.open(address, function (status) {
-    //console.log("inside page.open()");
     if (status !== 'success') {
         console.log('Failed to load', address);
-	phantom.exit();
+        phantom.exit();
     }
     window.setTimeout(function() {
-	console.log(page.content);
-	phantom.exit();
-    }, 4000);
+        console.log(page.content);
+        phantom.exit();
+    }, 4000); // 4 second timeout.
 });
+
+phantom.exit();
