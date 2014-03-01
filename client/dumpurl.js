dbd2aad8d511edd237a4b31604ed8667651f2a61
Jason White <jdwhite@menelos.com>
2014-02-28 21:28:47 -0600

    Formatting changes: indentation uses tabs and successive alignment uses spaces. This is the only way to preserve formatting after indentation. Else/ifelse blocks now start on new line after closing brace.
    Modified POD command paragraph directives so that non-POD verbatim blocks
    would be ignored by POD aware programs.
    
    Moved license to top of file.

--- client/dumpurl.js
+++ client/dumpurl.js
@@ -4,30 +4,38 @@
 // Jason White <jdwhite@menelos.com>
 // 11-Feb-2014
 //
+// Initial indentation of is done with tabs; successive alignment is done
+// with spaces. To format this code per your indentation preference, say,
+// 4 spaces per tab stop:
+//	  less -x4
+//	  nano -T4
+//	  vim (set tabstop=4 (also in .vimrc))
+//	  vi  (set ts=4 (also in .exrc))
+//
 var page = new WebPage(), address;
 
 if (phantom.args.length === 0) {
-    console.log('Usage: dumpurl.js {URL}');
-    phantom.exit();
+	console.log('Usage: dumpurl.js {URL}');
+	phantom.exit();
 }
 
 // http://stackoverflow.com/questions/16854788/phantomjs-webpage-timeout
 page.settings.resourceTimeout = 10000; // 10 seconds
 page.onResourceTimeout = function(e) {
-    console.log(e.errorCode);   // Probably be 408
-    console.log(e.errorString); // Probably be 'Network timeout on resource'
-    console.log(e.url);         // URL whose request timed out
-    phantom.exit(1);
+	console.log(e.errorCode);   // Probably be 408
+	console.log(e.errorString); // Probably be 'Network timeout on resource'
+	console.log(e.url);         // URL whose request timed out
+	phantom.exit(1);
 };
 
 address = encodeURI(phantom.args[0]);
 page.open(address, function (status) {
-    if (status !== 'success') {
-        console.log('Failed to load', address);
-        phantom.exit();
-    }
-    window.setTimeout(function() {
-        console.log(page.content);
-        phantom.exit();
-    }, 4000); // 4 second timeout.
+	if (status !== 'success') {
+		console.log('Failed to load', address);
+		phantom.exit();
+	}
+	window.setTimeout(function() {
+		console.log(page.content);
+		phantom.exit();
+	}, 4000); // 4 second timeout.
 });
