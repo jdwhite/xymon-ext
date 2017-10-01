88f5bb3730c6463701285cabf3c427acda689e77
Jason White <jdwhite@menelos.com>
2017-09-30 20:52:09 -0500

    Formatted with Perl::Tidy.

--- ext/client/smart_monitor.pl
+++ ext/client/smart_monitor.pl
@@ -4,6 +4,7 @@
 #
 # Jason White <jdwhite@menelos.com>
 #
+
 =begin License
 
 The MIT License (MIT)
@@ -56,13 +57,14 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 use strict;
 use warnings;
+
 #use Data::Dumper;
-use English; # $PROGRAM_NAME
+use English;    # $PROGRAM_NAME
 
 use File::Basename qw(dirname);
 use Cwd qw(abs_path);
-use lib dirname(abs_path $0) . '/lib/perl5';
-use lib dirname(abs_path $0) . '/../lib/perl5';
+use lib dirname( abs_path $0) . '/lib/perl5';
+use lib dirname( abs_path $0) . '/../lib/perl5';
 use XymonEXT;
 
 #
@@ -80,36 +82,38 @@ $main::message = "";
 #
 # Check SMART status of each device.
 #
-foreach my $device (sort &get_devices()) {
-	open(SMARTCTL, "${main::SUDO} ${main::SMARTCTL} -s on -i -H $device |") || die "$!";
-	$main::message .= "======================= $device =======================\n";
-	while(<SMARTCTL>) {
-		my($current_condition) = "";
-		next if /^(smartctl|Copyright) /o;
-		next if /^===/o;
-		next if /^\s*$/o;
+foreach my $device ( sort &get_devices() ) {
+    open( SMARTCTL, "${main::SUDO} ${main::SMARTCTL} -s on -i -H $device |" )
+        || die "$!";
+    $main::message
+        .= "======================= $device =======================\n";
+    while (<SMARTCTL>) {
+        my ($current_condition) = "";
+        next if /^(smartctl|Copyright) /o;
+        next if /^===/o;
+        next if /^\s*$/o;
 
-		if (/^SMART overall\-health.+:\s+(\S+)/io) {
-			my $status = $1;
+        if (/^SMART overall\-health.+:\s+(\S+)/io) {
+            my $status = $1;
 
-			if ($status eq "PASSED") {
-				$current_condition = "green";
-			}
-			else {
-				$current_condition = "red";
-			}
-		}
+            if ( $status eq "PASSED" ) {
+                $current_condition = "green";
+            }
+            else {
+                $current_condition = "red";
+            }
+        }
 
-		chomp;
-		$main::message .= $_;
-		if ($current_condition ne "") {
-			$main::message .= " &${current_condition}";
-			set_testcolor($current_condition);
-		}
-		$main::message .= "\n";
-	}
-	close(SMARTCTL);
-	$main::message .= "\n";
+        chomp;
+        $main::message .= $_;
+        if ( $current_condition ne "" ) {
+            $main::message .= " &${current_condition}";
+            set_testcolor($current_condition);
+        }
+        $main::message .= "\n";
+    }
+    close(SMARTCTL);
+    $main::message .= "\n";
 }
 
 #
@@ -127,9 +131,7 @@ foreach my $device (sort &get_devices()) {
 #   "summary"  - brief text shown on first line after date and test name
 #
 
-send_status(
-	"message"  => $main::message,
-);
+send_status( "message" => $main::message, );
 
 exit;
 
@@ -137,27 +139,28 @@ exit;
 # Get devices names.
 #
 sub get_devices {
-	my @devices = ();
-	my $OS = `uname`;
+    my @devices = ();
+    my $OS      = `uname`;
 
-	chomp($OS);
-	if ($OS =~ /.*BSD$/io) {
-		open(IOSTAT, "iostat -x |") || die "iostat: $!";
-		while(<IOSTAT>) {
-			next unless /^([ws]d\d+)/o;
-			push(@devices, "/dev/r${1}d");
-		}
-		close(IOSTAT);
-	}
-	elsif ($OS =~ /Darwin/) {
-		open(DISKUTIL, "diskutil list physical|") || die "$!";
-		while(<DISKUTIL>) {
-			next unless /^(\/dev\/disk\S+)/io;
-			push(@devices, $1);
-		}
-	} else {
-		warn "OS '$OS' not recognized. Update script.\n";
-	}
+    chomp($OS);
+    if ( $OS =~ /.*BSD$/io ) {
+        open( IOSTAT, "iostat -x |" ) || die "iostat: $!";
+        while (<IOSTAT>) {
+            next unless /^([ws]d\d+)/o;
+            push( @devices, "/dev/r${1}d" );
+        }
+        close(IOSTAT);
+    }
+    elsif ( $OS =~ /Darwin/ ) {
+        open( DISKUTIL, "diskutil list physical|" ) || die "$!";
+        while (<DISKUTIL>) {
+            next unless /^(\/dev\/disk\S+)/io;
+            push( @devices, $1 );
+        }
+    }
+    else {
+        warn "OS '$OS' not recognized. Update script.\n";
+    }
 
-	return @devices;
+    return @devices;
 }
